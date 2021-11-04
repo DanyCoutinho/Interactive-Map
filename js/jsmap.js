ed2a42427bc6f1079e9b55bca73bd7138b8fcf7d
Dany Coutinho <dany.coutinho@outlook.com>
2021-11-04 08:10:32 +0000

    JavaScript Externo
    Chamamento do JavaScript para o HTML feito com sucesso!

--- js/jsmap.js
+++ js/jsmap.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".marker-seixascaminha").click(function(){
-    $(".seixascaminha-txt").toggle();
-  });
+$(".marker-seixascaminha").hover(function(){
+$(".seixascaminha-txt").toggle();
+});
 });
